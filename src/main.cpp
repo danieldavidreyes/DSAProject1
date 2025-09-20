3f354dbc58f990958cbf36d3ffa1ce485f2ea931
dannybutbetter <137979149+danieldavidreyes@users.noreply.github.com>
2025-09-20 02:06:26 +0000

    Yes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+class AVLTree{
+
+	public:
+	void insert(int num){
+		
+	}
+	vector<int> inorder(){
+		return {};
+	}
+};
 
 int main(){
 	cout << "Hello AVL!\n";
